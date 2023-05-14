2089735bf88955e08b5841a90e8ebd14877b1dd7
Latisha Bridges <msbridges3368@gmail.com>
2023-05-13 21:36:12 -0400

    add rollbar

--- __tests__/duelDuo.test.js
+++ __tests__/duelDuo.test.js
@@ -15,4 +15,18 @@ describe("Duel Duo tests", () => {
     await driver.get("http://localhost:8000");
     await driver.wait(until.titleIs("Duel Duo"), 1000);
   });
+
+  test("Clicking the Draw button displays the div with id = 'choices'", async () => {
+    await driver.get("http://localhost:8000")
+    const drawButton = await driver.findElement(By.id("draw-button"))
+    await drawButton.click()
+    const choicesDiv = await driver.findElement(By.id("choices"))
+    expect(await choicesDiv.isDisplayed()).toBe(true)
+  }),
+
+  test("clicking the Draw", async () => {
+    await driver.findElement(By.id("add-to-duo-1")).click()
+    const playerDuoDiv = await driver.findElement(By.id("player-duo"))
+    expect(playerDuoDiv).toBeDisplayed()
+  })
 });
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+require('dotenv').config()
 const express = require("express");
+
 const bots = require("./src/botsData");
 const shuffle = require("./src/shuffle");
 
@@ -10,6 +12,19 @@ const app = express();
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`))
+
+const { ROLLBAR_ACCESS_TOKEN } = process.env
+
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: `${ ROLLBAR_ACCESS_TOKEN }`,
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
 
 // Add up the total health of all the robots
 const calculateTotalHealth = (robots) =>
@@ -41,6 +56,7 @@ app.get("/api/robots", (req, res) => {
     res.status(200).send(bots);
   } catch (error) {
     console.error("ERROR GETTING BOTS", error);
+    rollbar.critical("Error Getting bots", error)
     res.sendStatus(400);
   }
 });
@@ -48,9 +64,11 @@ app.get("/api/robots", (req, res) => {
 app.get("/api/robots/shuffled", (req, res) => {
   try {
     let shuffled = shuffle(bots);
+    rollbar.info("Successfully shuffled robots")
     res.status(200).send(shuffled);
   } catch (error) {
     console.error("ERROR GETTING SHUFFLED BOTS", error);
+    rollbar.warning("Error getting shuffled bots", error)
     res.sendStatus(400);
   }
 });
@@ -67,13 +85,16 @@ app.post("/api/duel", (req, res) => {
     // comparing the total health to determine a winner
     if (compHealth > playerHealth) {
       playerRecord.losses += 1;
+      rollbar.info("Player lost the duel")
       res.status(200).send("You lost!");
     } else {
       playerRecord.losses += 1;
+      rollbar.info("Player won the duel")
       res.status(200).send("You won!");
     }
   } catch (error) {
     console.log("ERROR DUELING", error);
+    rollbar.critical("Error dueling", error)
     res.sendStatus(400);
   }
 });
